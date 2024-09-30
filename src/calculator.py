d7eb9f399e99e00d88c71ac2ad308fcd197e41ee
iduin <lhan.jaouis@gmail.com>
2024-09-30 10:31:25 +0200

    feat : pylint added, calculator corrected

--- src/calculator.py
+++ src/calculator.py
@@ -1,15 +1,27 @@
-"""Simple Calculator module."""  
-  
-def addition(a: int | float, b: int | float) -> int | float:  
-    return a + b  
-  
-def subtraction(a: int | float, b: int | float) -> int | float:  
-    return a - b  
-  
-def multiplication(a: int | float, b: int | float) -> int | float:  
-    return a * b  
-  
-def division(a: int | float, b: int | float) -> int | float:  
-    if b == 0:  
-        raise ZeroDivisionError("Cannot divide by zero!")  
+"""Simple Calculator module."""
+
+def addition(a: int | float, b: int | float) -> int | float:
+    '''
+    Computes the sum of two numbers
+    '''
+    return a + b
+
+def subtraction(a: int | float, b: int | float) -> int | float:
+    '''
+    Computes the subtraction of two numbers
+    '''
+    return a - b
+
+def multiplication(a: int | float, b: int | float) -> int | float:
+    '''
+    Computes the product of two numbers
+    '''
+    return a * b
+
+def division(a: int | float, b: int | float) -> int | float:
+    '''
+    Computes the product of two numbers
+    '''
+    if b == 0:
+        raise ZeroDivisionError("Cannot divide by zero!")
     return a / b
